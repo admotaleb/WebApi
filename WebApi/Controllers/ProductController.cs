57bfb78c4a36b6f512f2d5b1b876c38d5f2d4598
Administrator <Administrator@ISBSERVER>
2022-07-21 15:09:02 +0600

    Add project files.

---
+++ WebApi/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        static List<String> Products = new List<string>()
+        {
+            "Mobile", "Laptop", "TV"
+        };
+
+        [HttpGet]
+        public List<string> GetProduct()
+        {
+            return Products;
+        }
+
+        [HttpGet]
+        public string GetProductById( int id)
+        {
+            return Products[id];
+        }
+
+       [HttpGet]
+
+       public void AddProduct(string Name)
+        {
+            Products.Add(Name);
+        }
+    }
+}
